d05162b159c274da6d8753de9637c8a6b14af26f
Desarrollo <desarrollo@gwspirits.com>
2020-12-30 16:54:25 -0500

    2020-12-30
    se maneja errores y se crea el crud

--- AngularApi/src/main/java/com/AngularApi/controllers/ClienteRestController.java
+++ AngularApi/src/main/java/com/AngularApi/controllers/ClienteRestController.java
@@ -1,9 +1,13 @@
 package com.AngularApi.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,43 +22,81 @@ import org.springframework.web.bind.annotation.RestController;
 import com.AngularApi.models.entity.Cliente;
 import com.AngularApi.models.services.IClienteService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
-	
+
 	@Autowired
 	private IClienteService clienteService;
-	
+
 	@GetMapping("/clientes")
-	public List<Cliente> index(){
+	public List<Cliente> index() {
 		return clienteService.findAll();
 	}
-	
+
 	@GetMapping("/clientes/{id}")
-	public Cliente show(@PathVariable Long id) {
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Cliente cliente = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			cliente = clienteService.findById(id);
+			
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en l Base de Datos");
+			response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if (cliente == null) {
+			response.put("mensaje", "El Cliente ID: ".concat(id.toString().concat(" No existe en la Base de Datos!")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);  
 	}
-	
+
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente create(@RequestBody Cliente cliente) {
-		return clienteService.save(cliente);
+	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+		Cliente clienteNew = null ;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			clienteNew = clienteService.save(cliente);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en l Base de Datos");
+			response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente se ha creado con éxito!");
+		response.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
-		
+	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
 		Cliente clienteActual = clienteService.findById(id);
+		Cliente clienteActualizado = null;
+		Map<String, Object> response = new HashMap<>();
+		if (clienteActual == null) {
+			response.put("mensaje", "Error no se pudo editar el Cliente ID: ".concat(id.toString().concat(" No existe en la Base de Datos!")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
 		
-		clienteActual.setApellido(cliente.getApellido());
-		clienteActual.setNombre(cliente.getNombre());
-		clienteActual.setEmail(cliente.getEmail());
-		
-		return clienteService.save(clienteActual);
+		try {
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setEmail(cliente.getEmail());
+			clienteActual.setCreateAt(cliente.getCreateAt());
+			clienteActualizado =  clienteService.save(clienteActual);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al actualizar el cliente en la  Base de Datos");
+			response.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "El cliente se ha actualizado con éxito!");
+		response.put("cliente", clienteActualizado);
+		return new ResponseEntity<Map<String, Object>>(response,HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("clientes/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
--- sitioAngular/src/app/clientes/clientes.component.ts
+++ sitioAngular/src/app/clientes/clientes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service'
+import swal from 'sweetalert2'
 
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
 
@@ -19,5 +20,30 @@ export class ClientesComponent implements OnInit {
       clientes => this.clientes = clientes
     );
   }
+  delete(cliente: Cliente): void{
+    swal.fire({
+      title: 'Está Seguro?',
+      text: `Seguro que desea eliminar el Cliente ${cliente.nombre} ${cliente.apellido}!`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Eliminar!',
+      cancelButtonText: 'No, Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ClienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes.filter(cli => cli != cliente)
+          swal.fire(
+            'Cliente Eliminado!',
+            `Cliente ${cliente.nombre} eliminado con éxito.`,
+            'success'
+          )
+        }
+        )
 
+      }
+    })
+  }
 }
